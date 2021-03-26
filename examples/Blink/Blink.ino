276ca547bb71ad456b76e42c421aeb9c86645b58
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-03-26 09:29:00 +0100

    Update Blink.ino

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -10,8 +10,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-    FastDigitalWrite(pinLed, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWriteFast(pinLed, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);                       // wait for a second
-    FastDigitalWrite(pinLed, LOW);    // turn the LED off by making the voltage LOW
+    digitalWriteFast(pinLed, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);                       // wait for a second
 }
